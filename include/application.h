9e486179fb52c66dc9d5ea6d367efd63e99ac457
XpulseIV <malteaxner363@gmail.com>
2022-05-06 22:26:42 +0200

    nearly finished with create_profile

--- include/application.h
+++ include/application.h
@@ -1,24 +1,45 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 
 inline bool exists (const std::string& name) {
     struct stat buffer{};   
     return (stat (name.c_str(), &buffer) == 0); 
 }
 
+inline std::string hash_string(std::string plain_text)
+{
+    return plain_text;
+}
+
+inline std::string prompt_user(const std::string& prompt)
+{
+    std::string result;
+    std::cout << prompt + ": ";
+
+    std::getline(std::cin, result);
+    std::cout << std::endl;
+        
+    return result;
+}
+
+struct credential
+{
+    std::string name;
+    std::string id;
+    std::string site;
+    std::string email;
+    std::string password;
+};
+
 struct profile_info
 {
     std::string name;
-    std::string encrypted_name;
-
     std::string email;
-    std::string encrypted_email;
-    
+    std::vector<std::pair<std::string, std::string>> extra_details;
     std::string master_password;
-    std::string encrypted_master_password;
-
-    
+    std::vector<credential> credentials;
 };
 
 class application
@@ -46,13 +67,42 @@ private:
 
     void create_profile()
     {
-        std::cout << "Please enter your name: " << std::endl;
-        std::getline(std::cin, profile_.name);
-
-        std::cout << "Please enter your email: " << std::endl;
-        std::getline(std::cin, profile_.email);
+        profile_.name = prompt_user("Please fill out your full name")[1];
+        profile_.email = prompt_user("Please fill out your preferred email")[1];
         
-        std::cout << "Please enter a secure master password: " << std::endl;
-        std::getline(std::cin, profile_.master_password);
+        if (prompt_user("Do you want to add any extra details about yourself? [Y]es, [N]o") == "Y")
+        {
+            bool add_more = true;
+            
+            while (add_more)
+            {
+                std::string detail = prompt_user("Name the detail");
+                std::string value = prompt_user("Provide a value for " + detail);
+
+                profile_.extra_details.emplace_back(detail, value);
+
+                if (prompt_user("DO you want to add more? [Y]es, [N]o") == "N")
+                {
+                    add_more = false;
+                }
+            }
+        }
+
+        const std::string temp_password = hash_string(prompt_user("Please fill out a strong master password"));
+        std::string temp_password2 = hash_string(prompt_user("Please re-enter your master password for confirmation"));
+
+        while (temp_password != temp_password2)
+        {
+            std::cout << "Error, the master passwords do not match. Please try again!" << std::endl;
+
+            temp_password2 = hash_string(prompt_user("Please re-enter your master password"));
+        }
+
+        profile_.master_password = temp_password;
+
+        if (prompt_user("Do you want to add some credentials? [Y]es, [N]o") == "Yes")
+        {
+            bool add_more = true;
+        }
     }
 };
