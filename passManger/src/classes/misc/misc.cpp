b628ff2ccfc08a12c0ac33bceae346665eaa878a
TuffenDuffen <reg_i@wext.se>
2022-05-07 17:05:58 +0200

    get-encryption-key (untested)

--- passManger/src/classes/misc/misc.cpp
+++ passManger/src/classes/misc/misc.cpp
@@ -1,8 +1,11 @@
 ﻿#include "misc.h"
 
 #include <iostream>
+#include <random>
 
 #include "../../libs/sha256.h"
+
+constexpr int encryptor_size = 16;
 
 std::string misc::prompt_user(const std::string prompt)
 {
@@ -18,4 +21,28 @@ std::string misc::prompt_user(const std::string prompt)
 std::string misc::hash_masterpassword(const std::string& password)
 {
     return sha256(password);
+}
+
+std::vector<int> misc::get_encryption_key(const std::string& password)
+{
+    // Create base randomizer
+    std::default_random_engine baseGen (password.length());
+    std::uniform_int_distribution<char> baseDist (16, 32);
+
+    // Create index randomizer
+    std::default_random_engine indexGen (password.length());
+    std::uniform_int_distribution<char> indexDist (0, password.length() - 1);
+
+    int length = baseDist(baseGen);
+
+    std::vector<int> result;
+    result.reserve(length);
+
+    for (int i = 0; i < length; i++)
+    {
+        std::default_random_engine generator (baseDist(baseGen) + password[indexDist(indexGen)]);
+        std::uniform_int_distribution<char> distribution(0, encryptor_size - 1);
+        result.push_back(distribution(generator));
+    }
+    return result;
 }
--- passManger/src/classes/misc/misc.h
+++ passManger/src/classes/misc/misc.h
@@ -1,9 +1,11 @@
 ﻿#pragma once
 #include <string>
+#include <vector>
 
 class misc
 {
 public:
     static std::string prompt_user(std::string prompt);
     static std::string hash_masterpassword(const std::string& password);
+    static std::vector<int> get_encryption_key(const std::string& password);
 };
